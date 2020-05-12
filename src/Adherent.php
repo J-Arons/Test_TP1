74e98ee250da2cbc4940dc746372e0ba4b08e557
JoGhost <47589470+JoGhost@users.noreply.github.com>
2020-05-12 16:04:20 +0200

    Commit test 3

--- src/Adherent.php
+++ src/Adherent.php
@@ -14,12 +14,24 @@ class Adherent
      */
     public function __construct(string $nom,string $prenom,DateTime $date_naissance)
     {
-        $this->identifiant=concatenateId($nom,$prenom,$date_naissance);
+        $this->identifiant=self::concatenateId($nom,$prenom,$date_naissance);
     }
 
     public static function concatenateId(string $nom, string $prenom, DateTime $date_naissance)
     {
-        return strtoupper($nom).strtoupper($prenom).date(DATE_ATOM, $date_naissance);
+        return strtoupper((self::skip_accents($nom)).strtoupper(self::skip_accents($prenom)).$date_naissance->format('Y-m-d'));
+    }
+
+    public static function skip_accents(string $string)
+    {
+        $charset='utf-8';
+        $str = htmlentities( $string, ENT_NOQUOTES, $charset );
+
+        $str = preg_replace( '#&([A-za-z])(?:acute|cedil|caron|circ|grave|orn|ring|slash|th|tilde|uml);#', '\1', $str );
+        $str = preg_replace( '#&([A-za-z]{2})(?:lig);#', '\1', $str );
+        $str = preg_replace( '#&[^;]+;#', '', $str );
+
+        return $str;
     }
 
 
