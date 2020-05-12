53ea6cb86162c5423e338d9ba32762dce49dcb5c
JoGhost <47589470+JoGhost@users.noreply.github.com>
2020-05-12 15:43:43 +0200

    Commit test 1 (Adherent.php)

--- src/Adherent.php
+++ src/Adherent.php
@@ -6,4 +6,14 @@ class Adherent
     private $identifiant;
 
 
+    /**
+     * Adherent constructor.
+     * @param string $nom
+     * @param string $prenom
+     * @param DateTime $date_naissance
+     */
+    public function __construct(string $nom,string $prenom,DateTime $date_naissance)
+    {
+        $this->identifiant=concatenateId($nom,$prenom,$date_naissance);
+    }
 }
